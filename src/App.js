98eda3a049014c881770d18fc5234631bd2a5865
Veshek <keshavkarthikk@gmail.com>
2023-08-01 12:44:09 -0700

    removed footer

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
                     <Route path='/VisaRescheduler' element={<USVisaRescheduler/>}/>
                 </Routes>
             <ScrollToTop/>
-            <Footer/>
+
         </div>
     )
 }
